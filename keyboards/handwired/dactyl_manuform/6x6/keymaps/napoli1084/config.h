f4269ca2cdada035e71e5b5cc01e78b1b83af212
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2021-05-19 13:04:37 -0400

    Add FRCAPS and F1F12 layers

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/config.h
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/config.h
@@ -26,7 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
-#define DEBOUNCE 30
+#undef DEBOUNCE
+#define DEBOUNCE 10
 
 #define RGBLED_SPLIT { 6, 6 }
 #define RGBLIGHT_ANIMATIONS
