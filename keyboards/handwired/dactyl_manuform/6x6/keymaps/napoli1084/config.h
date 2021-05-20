d116be025bee107218226e4e4ef5ea0d06ac63a9
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2021-05-20 13:23:44 -0400

    Add windows layer

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/config.h
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
 #undef DEBOUNCE
-#define DEBOUNCE 10
+#define DEBOUNCE 15
 
 #define RGBLED_SPLIT { 6, 6 }
 #define RGBLIGHT_ANIMATIONS
