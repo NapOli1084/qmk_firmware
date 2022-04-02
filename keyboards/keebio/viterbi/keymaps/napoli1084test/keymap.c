a04f9d3a408cb9bba8e2130feab61f4c77016da8
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2022-04-01 23:06:00 -0400

    Fixed RGBLED_NUM=14, small changes to QWERTY layer

--- keyboards/keebio/viterbi/keymaps/napoli1084test/keymap.c
+++ keyboards/keebio/viterbi/keymaps/napoli1084test/keymap.c
@@ -22,23 +22,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,------------------------------------------------.      ,------------------------------------------------.
- * |  Esc |   `  |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  |  -   | Bksp-|
+ * |  Esc |   `  |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  |  -   | Bksp |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |   ]  | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |   [  | Del  |
+ * |  \ | | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |   [  |  ]   |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |  F1  | Caps |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |   "  |Enter4|
+ * |  F1  | Caps |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |   "  |Enter |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |  F2  | Shift|   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /  | Shift|   \  |
+ * |  F2  | Shift|   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |  / ? | Shift|  =+  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Nap  | Ctrl |  \   | GUI  | Alt  | Num  |Space |      |Space |Raise | RAlt | App  |  Up  | Ctl  |   =  |
+ * | Nap  | Ctrl |  \"" | GUI  | Alt  | Num  |Space |      |Space |  F5  | RAlt | RGui | App  | Ctl  | Del  |
  * `------------------------------------------------'      `------------------------------------------------'
  */
   [_QWERTY] = LAYOUT_ortho_5x14(
     KC_ESC,  KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_BSPC,
-    KC_RBRC, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_DEL,
+    KC_BSLS, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
     KC_F1  , KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
-    KC_F2  , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_BSLS,
-    LOWER  , KC_LCTL, KC_BSLS, KC_LGUI, KC_LALT, LOWER  , KC_SPC ,     KC_SPC , KC_F4  , KC_RALT, KC_APP , KC_UP  , KC_RCTL, KC_EQL
+    KC_F2  , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_EQL ,
+    LOWER  , KC_LCTL, KC_NUBS, KC_LGUI, KC_LALT, LOWER  , KC_SPC ,     KC_SPC , KC_F5  , KC_RALT, KC_RGUI, KC_APP , KC_RCTL, KC_DEL
   ),
 
 /* Lower
