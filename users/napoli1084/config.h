aed4de9f054a502759eaac3ff423a05006c7bcb2
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2022-04-11 14:40:00 -0400

    Move my dacman keymap definitions to my user space

---
+++ users/napoli1084/config.h
@@ -0,0 +1,20 @@
+
+// how many taps before oneshot toggle is triggered
+// https://docs.qmk.fm/#/one_shot_keys?id=one-shot-keys
+#define ONESHOT_TAP_TOGGLE 2
+
+// how many taps before triggering the toggle with layer Tap-Toggle function TT(layer)
+// https://docs.qmk.fm/#/feature_layers?id=switching-and-toggling-layers
+#define TAPPING_TOGGLE 1
+
+// how long before a tap becomes a hold in milliseconds
+#define TAPPING_TERM 225
+
+// https://docs.qmk.fm/#/tap_hold?id=ignore-mod-tap-interrupt
+#define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+
+// Unicode mode WinCompose with KC_APP as trigger key
+// https://docs.qmk.fm/#/feature_unicode?id=setting-the-input-mode
+#define UNICODE_SELECTED_MODES UC_WINC
+// https://docs.qmk.fm/#/feature_unicode?id=input-key-configuration
+#define UNICODE_KEY_WINC KC_APPLICATION
