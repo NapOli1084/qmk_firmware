d3649dd68242c6f24a0e3d0a61aad966bbdd92c2
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2022-04-11 14:41:31 -0400

    Minor changes to my viterbi keymap

--- keyboards/keebio/viterbi/keymaps/napoli1084test/config.h
+++ keyboards/keebio/viterbi/keymaps/napoli1084test/config.h
@@ -24,6 +24,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define ONESHOT_TAP_TOGGLE 2
+#define TAPPING_TOGGLE 1
+#define IGNORE_MOD_TAP_INTERRUPT
+
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
