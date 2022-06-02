8b26045ee51cf6e864fbe84c9563430d3a3c16c3
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2022-06-02 07:30:48 -0400

    enable caps word, fix it with my tap dance and unicode

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 _______,_______,        _______,_______,
                                                 _______,_______,        _______,_______
     ),
-#endif // MOUSEKEY_ENABLE
+#endif // NAPOLI1084_MOUSE_LAYER_ENABLE
     [LYR_FN] = LAYOUT_6x6(
          NC_RSET, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, NC_RSET,
          _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
