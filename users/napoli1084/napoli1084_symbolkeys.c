1acee4172df668ac35f4ee793ec70179298702f1
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2022-06-06 22:13:33 -0400

    Remove my custom unicodemap_index() now that caps word was fixed for unicodemap

--- users/napoli1084/napoli1084_symbolkeys.c
+++ users/napoli1084/napoli1084_symbolkeys.c
@@ -148,35 +148,3 @@ bool napoli1084_process_symbol_mod(uint16_t keycode, keyrecord_t* record) {
     }
     return PROCESS_CONTINUE;
 }
-
-uint16_t unicodemap_index(uint16_t keycode) {
-    if (keycode >= QK_UNICODEMAP_PAIR) {
-        // Keycode is a pair: extract index based on Shift / Caps Lock state
-        uint16_t index = keycode - QK_UNICODEMAP_PAIR;
-
-        uint8_t mods = get_mods() | get_weak_mods();
-#ifndef NO_ACTION_ONESHOT
-        mods |= get_oneshot_mods();
-#endif
-
-        bool shift = mods & MOD_MASK_SHIFT;
-
-        //{napoli1084
-#ifdef CAPS_WORD_ENABLE
-        if (keycode >= a_CIRCM && keycode <= y_DIAER) {
-            shift |= is_caps_word_on();
-        }
-#endif
-        //}napoli1084
-
-        bool caps  = host_keyboard_led_state().caps_lock;
-        if (shift ^ caps) {
-            index >>= 7;
-        }
-
-        return index & 0x7F;
-    } else {
-        // Keycode is a regular index
-        return keycode - QK_UNICODEMAP;
-    }
-}
