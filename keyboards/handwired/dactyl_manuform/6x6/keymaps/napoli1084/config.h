48098d12a49c0532e29bee6ecc4cfd64b13fc199
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2022-06-11 13:49:18 -0400

    Add copyright notice in my files

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/config.h
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2021-2022 NapOli1084 (@napoli1084)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 
 #pragma once
 
