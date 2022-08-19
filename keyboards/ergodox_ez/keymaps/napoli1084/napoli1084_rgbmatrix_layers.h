f477d809145288c0d30acc28a57f3b087cbc37a2
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2022-08-19 13:17:59 -0400

    clean my rgb matrix maps

--- keyboards/ergodox_ez/keymaps/napoli1084/napoli1084_rgbmatrix_layers.h
+++ keyboards/ergodox_ez/keymaps/napoli1084/napoli1084_rgbmatrix_layers.h
@@ -18,8 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "napoli1084_colors.h"
 #include "ergodox_ez.h"
 
-#define NAPOLI1084_RGBMATRIX_LYR(...) {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}, {__VA_ARGS__}
-
 #define NAPOLI1084_RGBMATRIX_LYR_WINDOWS LED_LAYOUT_ergodox_pretty( \
     {HSV_RED}    ,{HSV_RED}    ,{HSV_WHITE}  ,{HSV_GREEN}  ,{HSV_GREEN}  ,    {HSV_RED}    ,{HSV_RED}    ,{HSV_WHITE}  ,{HSV_GREEN}  ,{HSV_GREEN}  ,  \
     {HSV_RED}    ,{HSV_RED}    ,{HSV_WHITE}  ,{HSV_GREEN}  ,{HSV_GREEN}  ,    {HSV_RED}    ,{HSV_RED}    ,{HSV_WHITE}  ,{HSV_GREEN}  ,{HSV_GREEN}  ,  \
