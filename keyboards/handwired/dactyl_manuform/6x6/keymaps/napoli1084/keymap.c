1003ab0576561c05212c80e685ac55eac95f321f
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2022-12-28 14:28:57 -0500

    dactyl tests, ergodox debug keys, dacman num keys fixes

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/keymap.c
@@ -44,12 +44,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 _______,KC_G   ,        _______,_______
     ),
     [LYR_NAVNUM] = LAYOUT_6x6(
-        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-        _______,KC_NO  ,KC_PSCR,KC_SLCK,KC_PAUS,KC_INS ,                        UN_CIRC,KC_NLCK,KC_SLSH,KC_ASTR,KC_MINUS,_______,
-        _______,KC_WH_L,KC_HOME,KC_UP  ,KC_PGUP,KC_WH_R,                        KC_PERC, KC_7  , KC_8  , KC_9  ,KC_MINUS,_______,
-        _______,KC_WH_U,KC_LEFT,KC_DOWN,KC_RGHT,KC_WH_U,                        KC_DLR , KC_4  , KC_5  , KC_6  ,KC_PLUS,_______,
-        _______,KC_WH_L,KC_END ,KC_WH_D,KC_PGDN,KC_WH_R,                        UN_HASH, KC_1  , KC_2  , KC_3  ,KC_EQL ,_______,
-                        _______,_______,                                                         KC_0  ,KC_DOT ,
+        _______,_______,_______,_______,_______,_______,                        _______,_______,NNRMDR5,NNRRGR5,_______,_______,
+        _______,KC_NO  ,KC_PSCR,KC_SLCK,KC_PAUS,KC_INS ,                        UN_CIRC,KC_NLCK,NNRMDR4,NNRRGR4,KC_MINUS,_______,
+        _______,KC_WH_L,KC_HOME,KC_UP  ,KC_PGUP,KC_WH_R,                        KC_PERC, KC_7  ,NNRMDR3,NNRRGR3,KC_MINUS,_______,
+        _______,KC_WH_U,KC_LEFT,KC_DOWN,KC_RGHT,KC_WH_U,                        KC_DLR , KC_4  ,NNRMDR2,NNRRGR2,KC_PLUS,_______,
+        _______,KC_WH_L,KC_END ,KC_WH_D,KC_PGDN,KC_WH_R,                        UN_HASH, KC_1  ,NNRMDR1,NNRRGR1,KC_EQL ,_______,
+                        _______,_______,                                                        NNRMDR0,NNRRGR0,
                                             _______,_______,                _______, OSFN   ,
                                                  _______,_______,    _______,_______,
                                                 _______,_______,        _______,_______
