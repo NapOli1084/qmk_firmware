c9861442e75c8ae79eb90f4d4a54fa7753e2d0f4
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2023-01-21 14:13:52 -0500

    dactyl keymap

--- keyboards/keebio/viterbi/keymaps/napoli1084/config.h
+++ keyboards/keebio/viterbi/keymaps/napoli1084/config.h
@@ -20,7 +20,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 
 #undef RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RGBLIGHT_EFFECT_BREATHING
+#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_ALTERNATING
+#undef RGBLIGHT_EFFECT_TWINKLE
+
+//#define RGBLIGHT_EFFECT_BREATHING // 4 sub-modes (speed), single color (initial red), slowly dim/raise brightness
+//#define RGBLIGHT_EFFECT_RAINBOW_MOOD // 3 sub-modes (speed), all leds same color, slowly changes hue
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL // 6 sub-modes (direction/speed), leds different color, hue moves right/left
+//#define RGBLIGHT_EFFECT_SNAKE // 6 sub-modes (direction/speed), leds off, then some light up single color move left/right
+//#define RGBLIGHT_EFFECT_KNIGHT // 3 sub-modes, similar to snake but both sides
+#define RGBLIGHT_EFFECT_CHRISTMAS // changing red/green
+//#define RGBLIGHT_EFFECT_STATIC_GRADIENT // 10 sub-modes, no anim, just different color gradients
+#define RGBLIGHT_EFFECT_RGB_TEST // all leds same color at same time, changing red/green/blue
+//#define RGBLIGHT_EFFECT_ALTERNATING // single color flashing on/off
+#define RGBLIGHT_EFFECT_TWINKLE // 6 sub-modes, leds off, then some slowly light on then off single/multi color randomly
 
 #define NAPOLI1084_QWERTY_ENABLE
