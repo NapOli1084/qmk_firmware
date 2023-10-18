dbba094512ac402e978f3c419806dbd8868ad8a6
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2023-10-17 20:49:44 -0400

    enable swap hands on dactyl

--- users/napoli1084/rules.mk
+++ users/napoli1084/rules.mk
@@ -2,7 +2,7 @@ CAPS_WORD_ENABLE = yes
 GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no
 # Mouse keys needed for wheel up/down/left/right
-MOUSEKEY_ENABLE = yes
+#MOUSEKEY_ENABLE = yes
 SPACE_CADET_ENABLE = no
 TAP_DANCE_ENABLE = yes
 UNICODEMAP_ENABLE = yes
