684b2c58579d92c40951d6c9c5d8942a17d4a519
NapOli1084 <10320176+NapOli1084@users.noreply.github.com>
2023-10-18 20:21:21 -0400

    enable swap hands on ergodox

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/rules.mk
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/napoli1084/rules.mk
@@ -1,3 +1,5 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = yes
 #RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 #VIA_ENABLE = yes
